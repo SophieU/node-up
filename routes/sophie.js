e7ac10444d0e5fa12e0f67c88dc98cd5c6317fac
wujing <j.u.sophia@qq.com>
2017-05-18 14:50:45 +0800

    node router

---
+++ routes/sophie.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.use(function (req, res, next) {
+    console.log(111)
+    next();
+})
+router.get('/', function (req, res, next) {
+    res.send('your are visition sophie')
+});
+
+router.get('/visit/:userId', function (req, res, next) {
+    let userId = req.params.userId;
+    res.json({'userId':userId})
+});
+router.get('/vectory/:id', function (req, res, next) {
+    let param = req.params;
+    res.json(param)
+})
+
+module.exports = router;
