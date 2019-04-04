8387e9e30821473c8cba6d0c0b0c3d7076bafde1
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2019-04-05 00:04:27 +0800

    file length

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ var fs = require('fs');
 
 fs.readdir(__dirname,function(err,files){
     console.log(files)
+    if(!files.length){
+        return console.log('\033[31m No files to show! \033[39m\n');
+    }
+    console.log(' Select which file or directory you want to see\n')
 })
