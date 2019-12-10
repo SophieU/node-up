04bb5e1a5883e3a40687e416f29ab173ed49c57e
SophieU <j.u.sophia@qq.com>
2019-12-10 11:04:31 +0800

    1210

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const Koa = require('koa')
 const router = require('koa-router')()
-const app = new Koa()
 const bodyParser = require('koa-bodyparser')
+const controllers = require('./controller')
+const nunjucks = require('nunjucks')
+
+const app = new Koa()
+
+// nunjucks
+function createEnv(path, opts) {
+    var 
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+        });
+        if (opts.filters) {
+            for (var f in opts.filters) {
+                env.addFilter(f, opts.filters[f]);
+            }
+        }
+        return env;
+}
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+var s = env.render('hello.html', { name: '小明' })
+console.log(s)
 // 一个中间件
 app.use(async (ctx, next) => {
     console.log('打印日志')
     await next()
 })
 app.use(bodyParser())
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name
-    ctx.response.body=`<h1>Hello, ${name}</h1>`
-})
-router.get(`/`, async (ctx, next) => {
-    ctx.response.body=`<h1>This is Index</h1>`
-})
-router.post('/signin', async (ctx, next) => {
-    var name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(ctx.request)
-    console.log(`signin with name :${name},password:${password}`)
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body=`<h1>Welcome,${name}</h1>`
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-})
+app.use(controllers())
 app.use(router.routes())
 
 app.listen(3000)
