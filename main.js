4dbb79b443e693191392763c2eebd4f202e0464a
SophieU <j.u.sophia@qq.com>
2021-03-30 10:05:46 +0800

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
 app.on('ready',function(){
     const win = new BrowserWindow({
         width: 300,
         height:300
     })
-    win.loadURL()
+    win.loadURL(path.join(__dirname, './index.html')) // mac file://, __dirname, './index.html'
 })
