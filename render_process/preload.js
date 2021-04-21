a6f7968bc2eac344f130993e17ba0f426d999707
SophieU <j.u.sophia@qq.com>
2021-04-21 10:26:58 +0800

    reset

--- render_process/preload.js
+++
@@ -1,5 +0,0 @@
-setTimeout(()=>{
-    document.querySelector('#su').onclick=function(){
-        alert('点击 了搜索')
-    }
-},5000)
--- render_process/render3.js
+++
@@ -1,9 +0,0 @@
-// 打开新窗口
-function openNewWindow(){
-    window.open('child.html', "子窗口")
-}
-
-window.addEventListener('message',function(event){
-    console.log(event)
-    let message = event.data
-},false)
